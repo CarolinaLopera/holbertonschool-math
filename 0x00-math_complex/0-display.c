a3bdb9b01d6fb66a27b9b0781137d793a2bf287f
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-30 15:28:04 -0500

    better version

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,18 +8,12 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re == 0 && c.im == 0)
-		return;
-
 	if (c.im == 0)
 		printf("%g\n", c.re);
-	else if (c.re == 0)
-		printf("%g\n", c.im);
-	else if (c.re < 0 && c.im < 0)
+	else if (c.im < 0)
 	{
-		c.re = c.re * -1;
 		c.im = c.im * -1;
-		printf("- %g - %gi\n", c.re, c.im);
+		printf("%g - %gi\n", c.re, c.im);
 	}
 	else
 		printf("%g + %gi\n", c.re, c.im);
