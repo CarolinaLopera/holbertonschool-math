35faeb764f2f48cdaa773d14d91079d7783ef0a2
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-30 11:25:09 -0500

    print complex number

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * display_complex_number - check the code for Holberton School students.
+ *
+ * Return: Always void.
+ * @c: is a variable double of structur complex.
+ */
+void display_complex_number(complex c)
+{
+	if (c.im == 0)
+		printf("%g\n", c.re);
+	else
+		printf("%g + %gi\n", c.re, c.im);
+}
---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+ * struct Complex - is a struct that get two doubles.
+ *
+ * @re: Is a variable double. Is real part to the number complex.
+ * @im: Is a variable double. Is imaginaty part to the number complex.
+ */
+typedef struct Complex
+{
+	double re;
+	double im;
+} complex;
+
+void display_complex_number(complex c);
+
+#endif
