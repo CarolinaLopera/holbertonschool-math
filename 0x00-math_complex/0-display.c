1cbb98d67828b8c202c2f1937399090baf1144be
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-30 19:37:10 -0500

    all validations

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -14,19 +14,15 @@ void display_complex_number(complex c)
 		printf("%g\n", c.re);
 	else if (c.re == 0)
 		printf("%g\n", c.im);
-
-	if (c.im < 0 && c.re < 0)
+	else if (c.im == -1)
 	{
-		c.im = c.im * -1;
-		c.re = c.re * -1;
-		printf("- %g - %gi\n", c.re, c.im);
+		printf("%g - i\n", c.re);
 	}
-	else if (c.re < 0)
+	else if (c.im == 1)
 	{
-		c.re = c.re * -1;
-		printf("%g - %gi\n", c.re, c.im);
+		printf("%g + i\n", c.re);
 	}
-	else if (c.im < 0)
+	else if (c.im < -1)
 	{
 		c.im = c.im * -1;
 		printf("%g - %gi\n", c.re, c.im);
